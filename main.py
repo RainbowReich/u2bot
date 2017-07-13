a7a367d3fad75e6c69881888c841f9d67119a121
RainbowReich <rainbowryke@gmail.com>
2017-07-13 01:45:01 -0500

    Remove unneeded comments

--- main.py
+++ main.py
@@ -1,19 +1,3 @@
-#!/usr/bin/python
-#
-# Copyright 2012 Google Inc. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import httplib2
 import sys
 
@@ -27,14 +11,12 @@ import time
 import datetime
 import string
 
-# For this example, the client id and client secret are command-line arguments.
 creds_file = open("credentials.json","r")
 creds = json.load( creds_file )
 client_id = creds['client_id']
 client_secret = creds['client_secret']
 creds_file.close()
 
-# The scope URL for read/write access to a user's calendar data
 scope = 'https://www.googleapis.com/auth/youtube.force-ssl'
 
 # Create a flow object. This object holds the client_id, client_secret, and
@@ -56,39 +38,16 @@ def main():
 
 def get_authenticated_service():
 
-  # Create a Storage object. This object holds the credentials that your
-  # application needs to authorize access to the user's data. The name of the
-  # credentials file is provided. If the file does not exist, it is
-  # created. This object can only hold credentials for a single user, so
-  # as-written, this script can only handle a single user.
   storage = Storage('credentials.dat')
 
-  # The get() function returns the credentials for the Storage object. If no
-  # credentials were found, None is returned.
   credentials = storage.get()
 
-  # If no credentials are found or the credentials are invalid due to
-  # expiration, new credentials need to be obtained from the authorization
-  # server. The oauth2client.tools.run_flow() function attempts to open an
-  # authorization server page in your default web browser. The server
-  # asks the user to grant your application access to the user's data.
-  # If the user grants access, the run_flow() function returns new credentials.
-  # The new credentials are also stored in the supplied Storage object,
-  # which updates the credentials.dat file.
   if credentials is None or credentials.invalid:
     credentials = tools.run_flow(flow, storage, tools.argparser.parse_args())
 
-  # Create an httplib2.Http object to handle our HTTP requests, and authorize it
-  # using the credentials.authorize() function.
   http = httplib2.Http()
   http = credentials.authorize(http)
 
-  # The apiclient.discovery.build() function returns an instance of an API service
-  # object can be used to make API calls. The object is constructed with
-  # methods specific to the calendar API. The arguments provided are:
-  #   name of the API ('calendar')
-  #   version of the API you are using ('v3')
-  #   authorized httplib2.Http() object that can be used for API calls
   return build('youtube', 'v3', http=http)
 
 def build_resource(properties):
@@ -156,7 +115,7 @@ def get_channel_id(youtube, video):
 
     return video_response["items"][0]["snippet"]["channelId"]
 
-# Post a comment to a video and upvote it instantly
+# Post a comment to a video
 def post_comment_and_upvote(youtube, video, comment):
     channel = get_channel_id(youtube, video)
     body = {
